4ac44d46d92b47f8c1dd3512b6bff687a99bbc90
Sami Lindgren <sami.lindgren@codecontrol.fi>
2023-07-17 13:12:55 +0300

    chunked upload start

--- apps/component-testing/src/pages/uploadTest.tsx
+++ apps/component-testing/src/pages/uploadTest.tsx
@@ -1,3 +1,23 @@
+import { useChunkUpload } from '@codecontrol/useChunkUpload'
 export default function UploadTest() {
-  return <>UploadTest</>
+  const { uploadFile } = useChunkUpload('http://localhost:5071/attachments', {
+    setFormData(formData, chunk, chunkedFile) {
+      formData.append('file', new Blob([chunk.blob], { type: chunkedFile.originalFile.type }), chunkedFile.originalFile.name)
+      formData.append('metadata.uploadUid', 'oudosta paikkaa keksitty arvo D:')
+      formData.append('metadata.chunkIndex', chunk.index.toString())
+      formData.append('metadata.totalChunks', chunkedFile.chunks.length.toString())
+      formData.append('metadata.totalFileSize', chunkedFile.originalFile.size.toString())
+    },
+  })
+
+  const handleUpload = (files: FileList | null) => {
+    if (!files) return
+    uploadFile(files)
+  }
+
+  return (
+    <div>
+      <input type='file' onChange={(file) => handleUpload(file.target.files)} />
+    </div>
+  )
 }
